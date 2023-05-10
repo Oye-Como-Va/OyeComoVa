2ab834c240706cf70a2cdb0f58d74bc365d02e09
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2023-05-10 10:03:34 +0200

    smtp

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -7,15 +7,18 @@ use Mail;
 
 class EmailController extends Controller
 {
-
-    public function contacto(Request $request){
-        $asunto = "Asunto del correo";//asunto
-        $para = "oyecomova2023@gmail.com";//correo que lo recibe
-        \Illuminate\Support\Facades\Mail::send('email',$request->all(), function($msj) use($asunto,$para){
-            $msj->from("oyecomova2023@gmail.com","Oyecomova");
-            $msj->subject($asunto);
-            $msj->to($para);
-        });
-        return redirect()->back();
+    
+        public function contacto(Request $request){
+            $asunto = "Asunto del correo";//asunto
+            $para = "oyecomova2023@gmail.com";//correo que lo recibe
+            \Illuminate\Support\Facades\Mail::send('email',$request->all(), function($msj) use($asunto,$para){
+                $msj->from("oyecomova2023@gmail.com","Oyecomova");
+                $msj->subject($asunto);
+                $msj->to($para);
+            });
+            return redirect()->back();
+        }
     }
-}
+
+
+
