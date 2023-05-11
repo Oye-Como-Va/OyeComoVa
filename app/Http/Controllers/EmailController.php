d04dfb50eac0c731761e3d56b6bb250e3574aa06
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2023-05-11 09:17:07 +0200

    kill smtp

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Mail;
+//use Mail;
+use Illuminate\Support\Facades\Mail;
 
 class EmailController extends Controller
 {
@@ -11,7 +12,7 @@ class EmailController extends Controller
         public function contacto(Request $request){
             $asunto = "Asunto del correo";//asunto
             $para = "oyecomova2023@gmail.com";//correo que lo recibe
-            \Illuminate\Support\Facades\Mail::send('email',$request->all(), function($msj) use($asunto,$para){
+            Mail::send('email',$request->all(), function($msj) use($asunto,$para){
                 $msj->from("oyecomova2023@gmail.com","Oyecomova");
                 $msj->subject($asunto);
                 $msj->to($para);
