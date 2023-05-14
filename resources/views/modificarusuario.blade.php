bc679cdfab4067f2e9ef098b7e92a0ced5901d88
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2023-05-14 19:24:40 +0200

    modificar

--- resources/views/modificarusuario.blade.php
+++ resources/views/modificarusuario.blade.php
@@ -1,18 +1,20 @@
 
 
-<div class="limiter">
+@extends('templates.loginTemplate')
+@section('register')
+    <div class="limiter">
 
         <div class="container-login100" style="background-image: url({{ URL::asset('fondo.webp') }});">
             <div class="wrap-login100">
                 <span class="login100-form-logo">
-                    <h1 class="brandH1">Formulario Editar perfil</h1>
+                    <h1 class="brandH1">Oye como va</h1>
                 </span>
                 <form method="POST" action="{{ route('login') }}">
                     @csrf
 
 
                     <span class="login100-form-title p-b-34 p-t-27">
-                        Editar
+                        Editar usuario
                     </span>
 
                     <div class="wrap-input100 validate-input">
@@ -22,7 +24,7 @@
                     </div>
 
                     <div class="wrap-input100 validate-input">
-                        <label for="surname">Apellidos</label>
+                        <label for="surname">Apellido</label>
                         <input  type="text" class="form-control" name="surname">
                     
                     </div>
@@ -36,17 +38,15 @@
                             </span>
                         @enderror
                     </div>
-
                     <div class="wrap-input100 validate-input">
                         <label for="phone">Telefono</label>
                         <input  type="text" class="form-control" name="phone">
                     
                     </div>
                     
-                    
                     <div class="container-login100-form-btn">
                         <button type="submit" class="login100-form-btn">
-                            {{ __('Login') }}
+                            Modificar
                         </button>
 
                     </div>
@@ -54,3 +54,24 @@
             </div>
         </div>
     </div>
+
+
+    <div id="dropDownSelect1"></div>
+
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/daterangepicker/moment.min.js"></script>
+    <script src="vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+    <script src="js/main.js"></script>
+@endsection
